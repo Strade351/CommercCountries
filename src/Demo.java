e6c120595cf589b5076c0bae9019fda052c0bd7e
timdrive <timdrive2013@gmail.com>
2017-09-04 13:44:36 +0400

    StringReader

--- src/Demo.java
+++ src/Demo.java
@@ -5,8 +5,8 @@ public class Demo {
     public static void main(String[] args) {
         Reader reader = new Reader();
         reader.reader();
-        for (int i = 0; i <reader.strings.size() ; i++) {
-            System.out.println(reader.strings.get(i));
+        for (int i = 0; i <reader.sstring.length ; i++) {
+            System.out.println(reader.sstring[i]);
         }
 
     }
--- src/Reader.java
+++ src/Reader.java
@@ -3,19 +3,27 @@ import java.util.ArrayList;
 
 public class Reader {
     int n;
-   ArrayList<String> strings = new ArrayList<>();
+   ArrayList<String> astrings = new ArrayList<>();
+   String[] sstring;
     public void reader(){
         try{
             FileInputStream fstream = new FileInputStream("countries.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
             String strLine;
             while ((strLine = br.readLine()) != null){
-                strings.add(strLine);
+                astrings.add(strLine);
 
             }
-            strings.remove(0);
+
+            astrings.remove(0);
+            sstring = new String[astrings.size()] ;
+            for (int i = 0; i < sstring.length ; i++) {
+                sstring[i] = astrings.get(i);
+
+            }
         }catch (IOException e){
             System.out.println("Ошибка");
+
         }
 
     }
