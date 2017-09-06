1d7b789701e25276e34e133a974dcd52093010b2
timdrive <timdrive2013@gmail.com>
2017-09-06 13:06:09 +0400

    PreFinal

--- src/HTMLGenerator.java
+++ src/HTMLGenerator.java
@@ -15,7 +15,7 @@ public class HTMLGenerator {
         File file = new File("countries.html");
         try {
             FileWriter fw = new FileWriter(file);
-            fw.append("<html> <title>Countries</title> <head<meta charset=\"utf-8\"> </head> <body> <table border=\"1\"> <tr> <th>Страна</th> <th>Численность населения</th> <th>Процент от мирового населения</th> </tr>");
+            fw.append("<html> <title>Countries</title> <head<meta charset=\"utf-8\"><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"> </head> <body> <table border=\"1\"> <tr> <th>Страна</th> <th>Численность населения</th> <th>Процент от мирового населения</th> </tr>");
             for (int i = 0; i < countries.length; i++) {
                 fw.write("<tr><td>" + countries[i].countryName + "</td><td>" + countries[i].peopleCount + "</td><td>" + countries[i].percent + "</td></tr>");
                 fw.append("\n");
@@ -31,12 +31,12 @@ public class HTMLGenerator {
         File file = new File("countriesIndex.html");
         try {
             FileWriter fw = new FileWriter(file);
-            fw.append("<html> <title>Countries</title> <head> </head> <body>");
+            fw.append("<html> <title>Countries</title> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"> </head> <body>");
             fw.write("<a href=\"#" + countries[0].countryName.charAt(3) + "\">" + countries[0].countryName.charAt(3) + "</a>\n");     //заголовок
             for (int i = 1; i < countries.length; i++) {
                 char temp = countries[i].countryName.charAt(3);
                 if (temp != countries[i - 1].countryName.charAt(3)) {
-                    fw.write("<a href=\"#" + temp + "\">" + temp + "</a> ");
+                    fw.write("<a name=\"Top\" href=\"#" + temp + "\">" + temp + "</a> ");
                 }
             }
 
@@ -46,13 +46,16 @@ public class HTMLGenerator {
                 char temp = countries[i].countryName.charAt(3);
                 if (i == 0 && temp == countries[i + 1].countryName.charAt(3)) {
                     fw.write("<a name=\"" + temp + "\">" + temp + "</a><br>");
+
                 }
                 else if (temp != countries[i - 1].countryName.charAt(3)) {
                     temp = countries[i + 1].countryName.charAt(3);
                     fw.write("<a name=\"" + temp + "\">" + temp + "</a><br>");
+                    fw.write("<p> <a href=\"#Top\">Top</a> </p>");
                 }
                 else
                     fw.write(countries[i].countryName + " <br>");
+
             }
             fw.append("</body></html>");
             fw.close();
