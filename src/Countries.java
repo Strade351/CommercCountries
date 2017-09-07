2c43e25740044fe470b53a6ec24b947181176f84
timdrive <timdrive2013@gmail.com>
2017-09-07 11:57:03 +0400

    Countries

--- src/Countries.java
+++ src/Countries.java
@@ -6,16 +6,18 @@ public class Countries {
     public Countries(ArrayList<Country> countries) {
         this.countries = countries;
     }
-    
+    /**
+     * <p>Возвращает по запрашиваемую страну по ее названию.</p>
+     * @param name Вертикаль, на которой находится фигура (1=a, 8=h)
+     *
+     * @return Найденную страну, если такая есть, если нет - возвращает null
+     */
     Country getCountry(String name){
         Country tmp = null;
         for (int i = 0; i < countries.size() ; i++) {
             if(countries.get(i).getCountryName().equals(name)){
                 tmp = countries.get(i);
             }
-            else return null;
-            
-            
         }
         return tmp;
         
