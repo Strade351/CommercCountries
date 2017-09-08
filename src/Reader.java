f8f54a30aff77ac628ba1ff68b5d040c487965ab
timdrive <timdrive2013@gmail.com>
2017-09-08 13:35:17 +0400

    Reader

--- src/Reader.java
+++ src/Reader.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Reader {
 
     ArrayList<String> astrings = new ArrayList<>();
+    ArrayList<String> cstrings = new ArrayList<>();
 
-    public void fillStrList() {
+
+    public void fillStrListCountries() {
         try {
             FileInputStream fstream = new FileInputStream("countries.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
@@ -22,12 +24,29 @@ public class Reader {
     }
 
     void fillContries(Countries c) {
-        fillStrList();
+        fillStrListCountries();
         for (int i = 1; i < astrings.size(); i++) {
             String[] tmp = astrings.get(i).split(" - ");
             String counts = tmp[1].replace(",", "");
             c.addCountry(tmp[0], Integer.parseInt(counts));
         }
+    }
+    public void fillStrListContinents() {
+        try {
+            FileInputStream fstream = new FileInputStream("continents.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            int count = 0;
+            while ((strLine = br.readLine()) != null) {
+                cstrings.add(strLine);
+                count++;
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка");
+        }
+    }
+    public void fillContinents(){
+
     }
 }
 
